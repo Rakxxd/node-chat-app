224f6a298f42691361ef1c26088de20bbccfd38c
Rakxxd <rakesh.emperor@gmail.com>
2017-09-12 23:35:45 +0530

    socket

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 //better way of specifying path
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
-
+//creating http server to enable use of socket.io
+var server = http.createServer(app);
+//configure socketIO
+var io = socketIO(server);
 //setting up static directory
 app.use(express.static(publicPath));
+//regitering event listener
+io.on('connection',(socket)=>{
+  console.log('New User connected');
 
-app.listen(port,()=>{
+  socket.on('disconnect',()=>{
+    console.log('User Disconnected');
+  });
+});
+server.listen(port,()=>{
   console.log("Server is up on ", port);
 });
