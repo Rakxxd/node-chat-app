2452d047daea65bb8da32cb042b1f4fe980fc5c4
Rakxxd <rakesh.emperor@gmail.com>
2017-09-13 00:01:20 +0530

    added_message

--- server/server.js
+++ server/server.js
@@ -17,6 +17,16 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New User connected');
 
+  socket.emit('newMessage',{
+    from: 'likemike@exapmle.com',
+    text:'Put your hands up',
+    createdAt : 123456
+  });
+
+  socket.on('createMessage',(message)=>{
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect',()=>{
     console.log('User Disconnected');
   });
