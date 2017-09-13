b57ea269bdf07749c0621f22aac07113333e6b2f
Rakxxd <rakesh.emperor@gmail.com>
2017-09-13 09:27:25 +0530

    newUser

--- server/server.js
+++ server/server.js
@@ -17,8 +17,17 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New User connected');
 
-
+  socket.emit('newMessage',{
+    from: 'Admin',
+    text: 'Welcome to App',
+    createdAt: new Date().getTime()
+  });
   //emits to single user
+  socket.broadcast.emit('newMessage',{
+    from:'Admin',
+    text:'New User',
+    createdAt: new Date().getTime()
+  })
   socket.on('createMessage',(message)=>{
     console.log('createMessage', message);
     //emits to all the connections
@@ -27,6 +36,11 @@ io.on('connection',(socket)=>{
       text: message.text,
       createdAt: new Date().getTime()
     })
+    // socket.broadcast.emit('newMessage',{
+    //     from: message.from,
+    //     text: message.text,
+    //     createdAt: new Date().getTime()
+    // })
   });
 
   socket.on('disconnect',()=>{
